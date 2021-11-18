9d0bba9bd28518537370b7c404b20fbb68d0723d
WooSoeb <bws96o@naver.com>
2021-11-18 18:48:34 +0900

    feat : match-subscribe, join 구현, user-session 멤버추가

--- src/match/match.gateway.ts
+++ src/match/match.gateway.ts
@@ -9,14 +9,21 @@ import {
   WsResponse,
   ConnectedSocket,
 } from "@nestjs/websockets";
-import { Logger } from "@nestjs/common";
+import { Inject, Logger } from "@nestjs/common";
 import { Server, Socket } from "socket.io";
 import { MatchService } from "./match.service";
 import { CreateMatchDto } from "./dto/create-match.dto";
+import { SubscribeCategoryDto } from "./dto/subscribe-category.dto";
+import { JoinMatchDto } from "./dto/join-match.dto";
+import { Match } from "./domain/match";
+import { IUserContainer } from "src/core/container/IUserContainer";
 
-@WebSocketGateway({ namespace: "/match" })
+@WebSocketGateway({ namespace: "/match", cors: { origin: "*" } })
 export class MatchGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private matchService: MatchService) {}
+  constructor(
+    private matchService: MatchService,
+    @Inject("IUserContainer") private userContainer: IUserContainer
+  ) {}
 
   @WebSocketServer() public server: Server;
   private logger: Logger = new Logger("MatchGateway");
@@ -31,6 +38,7 @@ export class MatchGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
 
   handleConnection(client: Socket, ...args: any[]) {
     this.logger.log(`Client connected: ${client.id}`);
+    this.logger.log(client.handshake.auth); // prints { token: "abcd" }
     // console.log("MatchGateway-connection!!", client.id);
   }
 
@@ -44,4 +52,58 @@ export class MatchGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
       data: this.matchService.createMatch(createMatchDto, client),
     };
   }
+
+  @SubscribeMessage("subscribe")
+  subscribe(
+    @MessageBody() subscribeCategoryDto: SubscribeCategoryDto,
+    @ConnectedSocket() client: Socket
+  ): MatchSubscribeAck {
+    if (
+      client.handshake.auth.token !=
+      this.userContainer.findById(subscribeCategoryDto.userId).sessionId
+    ) {
+      console.log(this.userContainer.findById(subscribeCategoryDto.userId).sessionId);
+      return {
+        status: 401,
+        data: [],
+      };
+    }
+    let matches: Match[] = this.matchService.subscribeByCategory(subscribeCategoryDto, client);
+    return {
+      status: 200,
+      data: matches.map((match): MatchInfo => {
+        return {
+          id: match.id,
+          shopName: match.shopName,
+          section: match.targetSection,
+          total: match.totalPrice,
+          tip: match.deliveryTip,
+        };
+      }),
+    };
+  }
+
+  @SubscribeMessage("join")
+  join(
+    @MessageBody() joinMatchDto: JoinMatchDto,
+    @ConnectedSocket() client: Socket
+  ): WsResponse<any> {
+    return {
+      event: "join",
+      data: this.matchService.joinMatch(joinMatchDto, client),
+    };
+  }
+}
+
+interface MatchSubscribeAck {
+  status: number;
+  data: MatchInfo[];
+}
+
+interface MatchInfo {
+  id: string;
+  shopName: string;
+  section: string;
+  total: number;
+  tip: number;
 }
--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,13 +1,15 @@
 import { Inject, Injectable } from "@nestjs/common";
-import { WsResponse } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
-import { User } from "../user/interfaces/user";
+import { IMatchContainer } from "src/core/container/IMatchContainer";
+import { SECTION } from "src/user/interfaces/user";
 import { IUserContainer } from "../core/container/IUserContainer";
-import { UserContainer } from "../core/container/UserContainer";
 import { MatchQueue } from "./domain/impl/MatchQueue";
 import { IMatchQueue } from "./domain/interfaces/IMatchQueue";
 import { Match, MatchBuilder } from "./domain/match";
 import { CreateMatchDto } from "./dto/create-match.dto";
+import { JoinMatchDto } from "./dto/join-match.dto";
+import { SubscribeCategoryDto } from "./dto/subscribe-category.dto";
+import { CATEGORY } from "./interfaces/category.interface";
 
 @Injectable()
 export class MatchService {
@@ -15,7 +17,11 @@ export class MatchService {
 
   public matchQueue: IMatchQueue = new MatchQueue();
 
-  constructor(@Inject("IUserContainer") private userContainer: IUserContainer) {}
+  constructor(
+    @Inject("IUserContainer") private userContainer: IUserContainer,
+    @Inject("IMatchContainer") private matchContainer: IMatchContainer,
+    @Inject("IMatchContainer") private closedMatchContainer: IMatchContainer
+  ) {}
 
   createMatch(createMatchDto: CreateMatchDto, client: Socket) {
     let match: Match = new MatchBuilder()
@@ -23,10 +29,49 @@ export class MatchService {
       .setDeliveryPriceAtLeast(createMatchDto.deliveryPriceAtLeast)
       .setDeliveryTipsInterval(createMatchDto.deliveryTipsInterval)
       .setPerchaser(this.userContainer.findById(createMatchDto.userId))
+      .setCategory(createMatchDto.category)
+      .setSection(createMatchDto.section)
       .build();
 
-    this.matchQueue.matchEnqueue(match);
+    this.matchContainer.push(match);
+
+    //해당 카테고리를 구독중인 클라이언트들에게 새로운 match가 있음을 통지
+    this.server.to(match.targetSection).emit("new-arrive", match);
 
     return { action: "create", status: "ok" };
+  }
+
+  closeMatchWait(matchId: string, client: Socket) {
+    //인증 필요
+    let closed = this.matchContainer.findById(matchId);
+    this.closedMatchContainer.push(closed);
+
+    //해당 카테고리를 구독중인 클라이언트들에게 기존 match가 종료되었음을 통지
+    this.server.to(closed.targetSection).emit("closed", closed);
+    this.matchContainer.delete(closed);
+  }
+
+  destroyMatch(matchId: string, client: Socket) {
+    let atContainer = this.matchContainer.findById(matchId);
+    if (atContainer) {
+      this.matchContainer.delete(atContainer);
+      return;
+    }
+
+    let atClosed = this.closedMatchContainer.findById(matchId);
+    if (atClosed) {
+      this.closedMatchContainer.delete(atClosed);
+    }
+  }
+
+  subscribeByCategory(subscribeCategoryDto: SubscribeCategoryDto, client: Socket) {
+    client.join(subscribeCategoryDto.category);
+    let matches: Match[] = this.matchContainer.findByCategory(subscribeCategoryDto.category);
+    return matches;
+  }
+
+  joinMatch(joinMatchDto: JoinMatchDto, client: Socket) {
+    let match: Match = this.matchContainer.findById(joinMatchDto.matchId);
+    // match.
   }
 }
