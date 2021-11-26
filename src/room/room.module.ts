2fd95141a9f2d89e5d1b91acd8945f5b3d2e8c5d
WooSoeb <bws96o@naver.com>
2021-11-27 01:47:27 +0900

    fix(module) : RoomSender providers 추가

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "src/auth/auth.module";
 import { ContainerModule } from "../core/container/container.module";
 import { RoomGateway } from "./room.gateway";
-import { RoomService } from './room.service';
+import { RoomSender } from "./room.sender";
+import { RoomService } from "./room.service";
 
 @Module({
   imports: [ContainerModule, AuthModule],
-  providers: [RoomGateway, RoomService],
+  providers: [RoomGateway, RoomService, RoomSender],
 })
 export class RoomModule {}
