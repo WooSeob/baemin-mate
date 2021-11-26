0617ced08cf1eb458de1d443f5f09a4e9adccc16
WooSoeb <bws96o@naver.com>
2021-11-27 00:51:06 +0900

    feat(dto) : from 메서드 추가

--- src/room/dto/response/room-view.dto.ts
+++ src/room/dto/response/room-view.dto.ts
@@ -1,3 +1,5 @@
+import { Match } from "src/match/domain/match";
+import { MenuItem } from "src/match/interfaces/shop.interface";
 import UserView from "./user-view.dto";
 
 export default class RoomView {
@@ -6,5 +8,24 @@ export default class RoomView {
   purchaser: UserView;
   tip: number;
   totalPrice: number;
-  users: UserView[];
+  users: {
+    user: UserView;
+    menus: MenuItem[];
+  }[];
+
+  static from(match: Match): RoomView {
+    const roomView = new RoomView();
+    roomView.matchId = match.id;
+    roomView.shopName = match.shopName;
+    roomView.purchaser = UserView.from(match.perchaser);
+    roomView.tip = match.deliveryTip;
+    roomView.totalPrice = match.totalPrice;
+    roomView.users = Array.from(match.sellectedMenus).map(([user, menuitems]) => {
+      return {
+        user: UserView.from(user),
+        menus: menuitems,
+      };
+    });
+    return roomView;
+  }
 }
--- src/room/dto/response/user-view.dto.ts
+++ src/room/dto/response/user-view.dto.ts
@@ -1,8 +1,16 @@
 import { MenuItem } from "src/match/interfaces/shop.interface";
+import { User } from "src/user/interfaces/user";
 
 export default class UserView {
   userId: string;
   nickname: string;
   mannerRate: number;
-  menus: MenuItem[];
+
+  static from(user: User): UserView {
+    const userView = new UserView();
+    userView.userId = user.getId();
+    userView.nickname = "not implemented";
+    userView.mannerRate = user.getMannerRate();
+    return userView;
+  }
 }
