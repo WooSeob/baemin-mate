fa45eef668f725a76783667a3d5d726c4c6e93a6
WooSoeb <bws96o@naver.com>
2021-11-28 22:34:14 +0900

    refactor : src/match/domain 삭제

--- src/match/domain/interfaces/IMatchQueue.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "src/user/interfaces/user";
-import { Room } from "../../../domain/room/room";
-
-export interface IMatchQueue {
-  matchEnqueue(match: Room);
-  matchDequeue(): Room;
-  joinerEnqueue(user: User);
-  joinerDequeue(): User;
-}
--- src/match/domain/interfaces/ISectionPriorityQueue.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "src/user/interfaces/user";
-import { Room } from "../../../domain/room/room";
-
-export interface ISectionPriorityQueue {
-  matchEnqueue(match: Room);
-  matchDequeue(): Room;
-  joinerEnqueue(user: User);
-  joinerDequeue(): User;
-}
