777d5570a17e621de8d2005bc1522d432cf8ecab
WooSoeb <bws96o@naver.com>
2022-05-12 00:32:04 +0900

    test : chat service 테스트코드 수정

--- src/chat/chat.service.spec.ts
+++ src/chat/chat.service.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ChatService } from "./chat.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { RoomEntity } from "../room/entity/room.entity";
-import { UserEntity } from "../user/entity/user.entity";
+import { RoomEntity, RoomRole } from "../room/entity/room.entity";
+import { UserBuilder, UserEntity } from "../user/entity/user.entity";
 import { UniversityEmailAuthEntity } from "../auth/entity/university-email-auth.entity";
 import { ParticipantEntity } from "../room/entity/participant.entity";
 import { MenuEntity } from "../room/entity/menu.entity";
@@ -14,42 +14,117 @@ import VoteOpinionEntity from "../room/entity/vote-opinion.entity";
 import RoomChatEntity from "../room/entity/room-chat.entity";
 import { RoomService } from "../room/room.service";
 import { EventEmitter } from "stream";
-import { RoomModule } from "../room/room.module";
 import { RoomEventType } from "../room/const/RoomEventType";
 
-import { Server as MockServer } from "mock-socket";
-import { Server } from "socket.io";
 import { UserService } from "../user/user.service";
-import { UserModule } from "../user/user.module";
-import { db } from "../../config";
+import { db_test } from "../../config";
+import { RoomAccountEntity } from "../room/entity/room-account.entity";
+import UniversityEntity from "../university/entity/university.entity";
+import { UserDeviceTokenEntity } from "../notification/entity/user-device-token.entity";
+import DormitoryEntity from "../university/entity/dormitory.entity";
+import { UserOauthEntity } from "../user/entity/user-oauth.entity";
+import { RoomGateway } from "../room/room.gateway";
+import UserChatMetadataEntity from "./entity/user-chat-metadata.entity";
 
-class MockRoomService extends EventEmitter {}
+const createMockRoomService = () => {
+  const eventEmitter = new EventEmitter();
+
+  const kicked = new Set();
+
+  eventEmitter.on(RoomEventType.USER_KICKED, (roomId, userId) => {
+    kicked.add(userId);
+  });
+
+  return {
+    on: (evName, listener) => {
+      eventEmitter.on(evName, listener);
+    },
+
+    emit: (evName, ...args) => {
+      eventEmitter.emit(evName, ...args);
+    },
+
+    getRoomRole: (roomId, userId) => {
+      return kicked.has(userId) ? RoomRole.BANNED : RoomRole.MEMBER;
+    },
+  };
+};
 const MockRoomServiceProvider = {
   provide: RoomService,
-  useClass: MockRoomService,
+  useValue: createMockRoomService(),
 };
 
-describe("ChatService", () => {
+const MockRoomGatewayProvider = {
+  provide: RoomGateway,
+  useValue: {
+    broadcastChat: jest.fn(),
+  },
+};
+
+const MockUserServiceProvider = {
+  provide: UserService,
+  useValue: {
+    on: jest.fn(),
+    findUserOrUnknownIfNotExist: (userId: string) => {
+      return new UserBuilder().setId(userId).setName("(테스트)").build();
+    },
+  },
+};
+
+const getTestingModule = async () => {
+  return await Test.createTestingModule({
+    imports: [
+      TypeOrmModule.forRoot({
+        ...db_test,
+        keepConnectionAlive: false,
+        entities: [
+          RoomEntity,
+          ParticipantEntity,
+          MenuEntity,
+          UserEntity,
+          ImageFileEntity,
+          RoomAccountEntity,
+          UniversityEntity,
+          MatchEntity,
+          UserDeviceTokenEntity,
+          DormitoryEntity,
+          UserOauthEntity,
+          UniversityEmailAuthEntity,
+          RoomBlacklistEntity,
+          RoomChatEntity,
+          RoomVoteEntity,
+          VoteOpinionEntity,
+          UserChatMetadataEntity,
+        ],
+        logging: ["query"],
+      }),
+      TypeOrmModule.forFeature([RoomChatEntity, UserChatMetadataEntity]),
+    ],
+    providers: [
+      ChatService,
+      MockRoomServiceProvider,
+      MockRoomGatewayProvider,
+      MockUserServiceProvider,
+    ],
+  }).compile();
+};
+
+describe("조회 테스트", () => {
   let mockRoomService: RoomService;
   let service: ChatService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot(db),
-        TypeOrmModule.forFeature([RoomChatEntity]),
-        UserModule,
-      ],
-      providers: [ChatService, MockRoomServiceProvider],
-    }).compile();
+  const roomId = "d0e65ffd-92c7-44e6-921f-f4d225c6115f";
+  const userOne = "0e7660e5-3949-4ca2-93ab-b71eca6892da";
+  const userTwo = "48ce7d82-e92b-43b0-ae42-5997bdd288ab";
 
+  beforeAll(async () => {
+    const module: TestingModule = await getTestingModule();
     service = module.get<ChatService>(ChatService);
     mockRoomService = module.get<RoomService>(RoomService);
-    await service.clear();
-    // service.server = new MockServer("");
+  });
 
-    const fakeURL = "ws://localhost:8080";
-    // const mockServer = ;
+  beforeEach(async () => {
+    await service.clear();
   });
 
   it("should be defined", () => {
@@ -57,13 +132,48 @@ describe("ChatService", () => {
     expect(mockRoomService).toBeDefined();
   });
 
-  it("유저 입장 메시지", async () => {
-    const roomId = "f9c20621-a116-413a-a3dc-d395151847fa";
-    mockRoomService.emit(RoomEventType.USER_ENTER, roomId, "abdc");
+  it("입장 전의 메시지는 받아올 수 없다.", async () => {
+    mockRoomService.emit(RoomEventType.USER_ENTER, roomId, userOne);
     await aFewSecondsLater(1000);
-    const messages = await service.getAllMessagesByRoom(roomId);
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "1");
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "2");
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "3");
+
+    mockRoomService.emit(RoomEventType.USER_ENTER, roomId, userTwo);
+    await aFewSecondsLater(1000);
+
+    const messages = await service.getAllMessagesByRoomForUser(roomId, userTwo);
+
     expect(messages).toHaveLength(1);
-    console.log(messages);
+  });
+
+  it("강제퇴장 후 발생한 메시지는 받아올 수 없다.", async () => {
+    mockRoomService.emit(RoomEventType.USER_ENTER, roomId, userOne);
+
+    mockRoomService.emit(RoomEventType.USER_ENTER, roomId, userTwo);
+    await aFewSecondsLater(1000);
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "1");
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "2");
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "3");
+    mockRoomService.emit(RoomEventType.USER_KICKED, roomId, userTwo);
+
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "4");
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "5");
+    mockRoomService.emit(RoomEventType.CHAT, roomId, userOne, "6");
+
+    await aFewSecondsLater(1000);
+
+    const messagesForUserOne = await service.getAllMessagesByRoomForUser(
+      roomId,
+      userOne
+    );
+    expect(messagesForUserOne).toHaveLength(9);
+
+    const messagesForUserTwo = await service.getAllMessagesByRoomForUser(
+      roomId,
+      userTwo
+    );
+    expect(messagesForUserTwo).toHaveLength(5);
   });
 });
 
