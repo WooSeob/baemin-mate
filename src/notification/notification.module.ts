6dfc05358443e03dce032876189eea52670d6a62
WooSoeb <bws96o@naver.com>
2022-06-10 01:47:47 +0900

    feat : 알림 조회, 읽음 처리 추가

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -6,11 +6,13 @@ import { UserDeviceTokenEntity } from "./entity/user-device-token.entity";
 import { FcmModule } from "../infra/fcm/fcm.module";
 import { NotificationController } from "./notification.controller";
 import { AuthModule } from "../auth/auth.module";
+import { NotificationEntity } from "./entity/notification.entity";
 
 @Module({
   imports: [
     forwardRef(() => RoomModule),
     TypeOrmModule.forFeature([UserDeviceTokenEntity]),
+    TypeOrmModule.forFeature([NotificationEntity]),
     FcmModule,
     forwardRef(() => AuthModule),
   ],
