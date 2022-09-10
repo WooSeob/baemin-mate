4816055e9f1fd5c61d27d4d6288e3f0b7c26e657
WooSoeb <bws96o@naver.com>
2022-09-10 18:25:46 +0900

    fix : jwtService -> jwt 모듈 변경

--- src/auth/oauth/OAuthModule.ts
+++ src/auth/oauth/OAuthModule.ts
@@ -2,23 +2,8 @@ import { Module } from "@nestjs/common";
 import { OAuthService } from "./o-auth.service";
 import { NaverOAuthProviderService } from "./providers/NaverOAuthProviderService";
 import { AppleOAuthProviderService } from "./providers/apple/AppleOAuthProviderService";
-import { JwtModule } from "@nestjs/jwt";
-import { AppleOAuthConfig } from "../../../config";
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: AppleOAuthConfig.JwtConfig.getSecret(),
-      signOptions: {
-        expiresIn: AppleOAuthConfig.JwtConfig.defaultExpIn,
-        algorithm: "RS256",
-        keyid: AppleOAuthConfig.JwtConfig.keyid,
-        issuer: AppleOAuthConfig.TEAM_ID,
-        audience: AppleOAuthConfig.DOMAIN,
-        subject: AppleOAuthConfig.CLIENT_ID,
-      },
-    }),
-  ],
   providers: [
     OAuthService,
     NaverOAuthProviderService,
